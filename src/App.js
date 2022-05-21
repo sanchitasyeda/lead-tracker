8dfad57a108ae33b01ea6a762eb31b6ec90ecdf2
Sanchita <sanchitasyeda@gmail.com>
2022-05-20 21:32:12 -0700

    Added Form Functionality to Add Form Inputs to Table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 import data from "./mock-data.json";
 
 const App = () => {
 
   const [contacts, setContacts] = useState(data); // taking data and storing it in state
+  const [addFormData, setAddFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    program: "",
+    tags: "",
+    notes: ""
+  })
+
+  // function to handle form input
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name"); // will get name attribute of whatever input user has typed
+    const fieldValue = event.target.value; // actual value
+
+    const newFormData = { ...addFormData}; // copy of existing form data without mutating state
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      name: addFormData.name,
+      address: addFormData.address,
+      phoneNumber: addFormData.phoneNumber,
+      email: addFormData.email,
+      program: addFormData.program,
+      tags: addFormData.tags,
+      notes: addFormData.notes
+    }
+
+    const newContacts = [...contacts, newContact]
+    setContacts(newContacts);
+  }
 
   return <div className="app-container">
     <table>
@@ -31,22 +72,44 @@ const App = () => {
       </tbody>
     </table>
     <h2>Add a Contact</h2>
-    <form>
+    <form onSubmit={handleAddFormSubmit}>
       <input type="text"
         name="name"
         required="required"
         placeholder="Enter a name"
+        onChange={handleAddFormChange}
       />
       <input type="text"
         name="phoneNumber"
         required="required"
         placeholder="Enter a phone number"
+        onChange={handleAddFormChange}
       />
       <input type="text"
         name="email"
         required="required"
         placeholder="Enter an email"
+        onChange={handleAddFormChange}
       />
+        <input type="text"
+        name="program"
+        required="required"
+        placeholder="Enter a program"
+        onChange={handleAddFormChange}
+      />
+        <input type="text"
+        name="tags"
+        required="required"
+        placeholder="Enter any tags"
+        onChange={handleAddFormChange}
+      />
+        <input type="text"
+        name="notes"
+        required="required"
+        placeholder="Enter notes"
+        onChange={handleAddFormChange}
+      />
+      <button type="submit">Add</button>
     </form>
   </div>
 
